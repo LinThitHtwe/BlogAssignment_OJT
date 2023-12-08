0fbfc2e9e8f5b94a2fadd4a94c23989dbd039c7f
LinThit27 <linthit2745@gmail.com>
2023-12-08 15:40:00 +0630

    [modify] nav bar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const NavbarComponent = () => {
   return (
     <Navbar
       bg="white"
       expand="lg"
       variant="dark"
-      className="d-flex justify-content-between px-5 navbar"
+      className="d-flex justify-content-between px-2 px-lg-5 navbar"
     >
-      <Navbar.Brand href="/" className="display-5 navbar-brand">
+      <Navbar.Brand href="/" className="display-5 w-25 navbar-brand">
         NORDIC ROSE
       </Navbar.Brand>
-
-      <Nav className="ml-auto nav ">
-        <Nav.Link className="nav-link" href="#">
-          BLOG
-        </Nav.Link>
-        <Nav.Link className="nav-link" href="#">
-          ABOUT
-        </Nav.Link>
-        <Nav.Link className="nav-link" href="#">
-          LINKS
-        </Nav.Link>
-        <Nav.Link className="nav-link" href="#">
-          PROJECTS
-        </Nav.Link>
-      </Nav>
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        style={{ color: "#000" }}
+      />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto  ">
+          <Nav.Link className="nav-link" href="#">
+            BLOG
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="#">
+            ABOUT
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="#">
+            LINKS
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="#">
+            PROJECTS
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
